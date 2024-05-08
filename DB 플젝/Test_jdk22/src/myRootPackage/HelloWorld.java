1ebf2d86b0306e2fef7064163f9b77c4ad61a8af
Rachi3 <rlagusdn715@gmail.com>
2024-05-08 22:45:41 +0900

    이클립스 테스트 및 개발 환경 구축

---
+++ DB 플젝/Test_jdk22/src/myRootPackage/HelloWorld.java
@@ -0,0 +1,10 @@
+package myRootPackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JDK 22로 다시 쓰는 Hello World");
+	}
+
+}
